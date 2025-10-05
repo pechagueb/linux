e10d60ae81abfb1091c0c572165094be6f6a86b0
pechagueb <52633085+pechagueb@users.noreply.github.com>
2025-10-05 11:47:20 +0100

    Update rsync-out.sh

--- rsync-out.sh
+++ rsync-out.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copias antes de cerrar sesión
+# Copias a unidad externa antes de cerrar sesión
 rsync -a --delete ~/Imágenes/ /mnt/0416CE2846AE4C1D/docs/Imágenes
 rsync -a --delete ~/Documentos/ /mnt/0416CE2846AE4C1D/docs/Documentos
 rsync -a --delete ~/Vídeos/ /mnt/0416CE2846AE4C1D/docs/Vídeos
